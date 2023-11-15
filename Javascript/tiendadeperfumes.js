ac33623eb9e2178429e7451e81d0e2d07c08edd7
Bruno Bonomo <bruno.bonomo11@gmail.com>
2023-11-14 21:22:42 -0300

    Cambios actualizados y codigo mas prolijo en Archivo JS

--- Javascript/tiendadeperfumes.js
+++ Javascript/tiendadeperfumes.js
@@ -1,122 +1,3 @@
-/*const headercarrito = document.querySelector("#header");
-const contenedorcarrito = document.querySelector("contenedorcarrito");
-const bodycarrito = document.querySelector("bodycarrito");
-
-window.addEventListener("scroll", function(){
-    if(contenedorcarrito.getBoundingClientRect().top<10){
-        headercarrito.classList.add("scroll")
-    }
-    else{
-        headercarrito.classList.remove("scroll")
-    }
-}) */
-
-/*alert("BIENVENIDOS A TIENDA DE PERFUMES")
-
-let nombreIngresado = prompt("Ingrese su Nombre");
-let apellidoIngresado = prompt("Ingrese su Apellido")
-let espacio = " ";
-
-if ((nombreIngresado != "") && (apellidoIngresado !="")){
-    alert("Nombre: "+nombreIngresado + espacio + "Apellido: "+apellidoIngresado);
-}
-else{
-    alert("Error: Ingresar nombre y apellido");
-}
-
-console.log("Bienvenido" + espacio + nombreIngresado)
-
-let marcaIngresada = prompt("Seleccione una marca ej Paco Rabanne")
-console.log("Usted ha seleccionado Paco Rabanne")
-
-const arrayMarcas = ["","DOLCE & GABANNA","AZZARO","PACO RABANNE","TOM FORD","JEAN PAUL GAULTIER"];
-arrayMarcas.push("YVES SAINT LAURENT")
-// console.log(arrayMarcas.length);
-
-
-let perfumeIngresado = prompt("Ingrese un Perfume ej Azzaro");
-console.log("Usted ha seleccionado" + espacio + perfumeIngresado)
-
-if (perfumeIngresado == "") {
-    alert("No ingresaste ningún producto");
-}
-else{
-    alert("Seleccionaste" + espacio + perfumeIngresado);
-}
-
-let entrada = prompt("Seleccione otro perfume")
-while(entrada != "exit"){
-    alert("El usuario ingresó "+ entrada);
-    entrada = prompt("Seleccione un nuevo perfume")
-}
-
-console.log("Escriba un perfume a seleccionar")
-
-for (let i = 0; i < 10; i++) {
-    if (i == 8){
-        break;
-    }
-    console.log(i);
-
-}*/
-
-// SOLICITAMOS AL USUARIO QUE INGRESE SUS DATOS PARA HACER UNA COMPRA EN LA TIENDA
-
-async function autenticar() {
-    const {
-      value: formAutenticar
-    } = await Swal.fire({
-      title: 'Ingrese su nombre de usuario',
-      html: '<div class="form-inline col-sm-12 mt-3">' +
-        '<label class="control-label col-sm-4" for="UserName">Usuario</label>' +
-        '<input required="" class="form-control col-sm-7" id="UserName" name="usuario" type="text" autofocus style="color: #2e7d32;">' +
-        '</div>' +
-        '<div class="form-inline col-sm-12 mt-3">' +
-        '<label class="control-label col-sm-4" for="UserPass">Contraseña</label>' +
-        '<input required="" class="form-control col-sm-7" id="UserPass" name="clave" type="password" style="color: #2e7d32;">' +
-        '</div>',
-      backdrop: false,
-      showConfirmButton: true,
-      showCancelButton: true,
-      confirmButtonText: 'Validar',
-      cancelButtonText: 'Cancelar',
-      confirmButtonColor: '#40B340',
-      cancelButtonColor: '#FF0000',
-      showCloseButton: true,
-      focusConfirm: true,
-      focusCancel: false,
-      preConfirm: () => {
-        return [
-          document.getElementById('UserName').value,
-          document.getElementById('UserPass').value
-        ]
-      },
-      onOpen: (modal) => {
-        confirmOnEnter = (event) => {
-          if (event.keyCode === 13) {
-            event.preventDefault();
-            modal.querySelector(".swal2-confirm").click();
-          }
-        };
-        modal.querySelector("#UserName").addEventListener("keyup", confirmOnEnter);
-        modal.querySelector("#UserPass").addEventListener("keyup", confirmOnEnter);
-      }
-    });
-    if (formAutenticar && formAutenticar[0] != '' && formAutenticar[1] != '') {
-      Swal.fire({
-        title: 'Datos Correctos',
-        icon: 'success',
-        backdrop: false
-      });
-    } else {
-      Swal.fire({
-        title: 'Datos incorrectos',
-        icon: 'warning',
-        backdrop: false
-      });
-    }
-  }
-
 // AGREGAMOS SWEET ALERT
 
 Swal.fire({
@@ -182,7 +63,7 @@ for (const objeto of almacenados)
 
 
 
-    // let contenedorcompras = document.getElementById("contenedorcompras");
+// let contenedorcompras = document.getElementById("contenedorcompras");
 // console.log(contenedorcompras.innerHTML);
 
 //let precioproducto1 = document.getElementsByClassName("precioproducto1");
@@ -239,13 +120,14 @@ console.log(mensaje);
 
 
 // STRINGIFY
-
+/*
 const fragancia1 = {id: 1, fragancia: "The one gold EDP", precio: 108520, esencia: "amaderada"};
 const enJSON = JSON.stringify(fragancia1);
 
 console.log(enJSON);
 console.log(typeof fragancia1);
 console.log(typeof enJSON);
+
 
 localStorage.setItem("fragancia1", enJSON);
 
@@ -266,6 +148,7 @@ console.log(typeof fragancia3);
 console.log(typeof enJSON3);
 
 localStorage.setItem("fragancia3", enJSON3);
+*/
 
 // PARSE
 
@@ -280,7 +163,7 @@ const fragancia6 = JSON.parse(localStorage.getItem("fragancia3"));
 
 console.log(fragancia6.id); */
 
-console.dir(document.body)
+/* console.dir(document.body) */
 
 
 
@@ -303,6 +186,7 @@ if (usuarioEnLS) {
 
 // SET TIME OUT
 
+/*
 console.log("Inicia proceso")
 
 setTimeout(() => {
@@ -310,6 +194,7 @@ setTimeout(() => {
 }, 2000)
 
 console.log("Fin proceso")
+*/
 
 
 
